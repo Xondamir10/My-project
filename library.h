7d636d67396e50296961274a4c094ee3984f7af6
Xondamir10 <x.nabijonov@newuu.uz>
2025-01-23 15:16:05 +0500

    commit

--- library.h
+++ library.h
@@ -1,6 +1,8 @@
-#ifndef CLIONPROJECTS_LIBRARY_H
-#define CLIONPROJECTS_LIBRARY_H
-
-void hello();
-
-#endif //CLIONPROJECTS_LIBRARY_H
+#include <iomanip>
+int main(){
+    int name;
+    std::cout << "Please,enter your name: ";
+    std::cin >> name;
+    std::cout << "Now please enter your age " << name << "years old" << std::endl;
+    return 0;
+}
