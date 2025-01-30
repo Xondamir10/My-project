3b7626c2f78728f31fbe747029adb84094d43aa5
Xondamir10 <x.nabijonov@newuu.uz>
2025-01-30 15:35:31 +0500

    commit

--- library.h
+++ library.h
@@ -1,8 +1,6 @@
-#include <iomanip>
-int main(){
-    int name;
-    std::cout << "Please,enter your name: ";
-    std::cin >> name;
-    std::cout << "Now please enter your age " << name << "years old" << std::endl;
-    return 0;
-}
+#include <iostream>
+using namespace std;
+int  a,b,c,d,e ;
+    cin >>a>>b>>c>>d>>e,
+    int sum=a+b+c+d+e,
+        cout<< "Sum is" <<sum>> endl;
