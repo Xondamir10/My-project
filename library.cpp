29101fa37c859f2ae6743c4b70e242780edf4138
Xondamir10 <x.nabijonov@newuu.uz>
2025-03-20 14:02:19 +0500

    commit

--- library.cpp
+++ library.cpp
@@ -1,250 +1,284 @@
 #include <iomanip>
 #include <iostream>
+#include <climits>
 #include <vector>
-#include <string>
+#include <cstdlib>
+#include <ctime>
    using namespace std ;
+//problem 1
+//  int main() {
+// int n;
+// std::cout << "Enter the number of integers (n): ";
+// std::cin >> n;
+//      if (n <= 0) {
+//     std::cout << "Please enter a positive integer for n." << std::endl;
+//     return 1;
+// }
+//      int numbers[n];
+//      std::cout << "Enter " << n << " integers:" << std::endl;
+// for (int i = 0; i < n; ++i) {
+//     std::cin >> numbers[i];
+// }
+// int sum = 0;
+// for (int i = 0; i < n; ++i) {
+//     sum += numbers[i];
+// }
+//      std::cout << "The sum of the integers is: " << sum << std::endl;
 
-   //problem 1
-// int recursiveProduct(int a, int b) {
-//     if (b == 0) {
-//         return 0;
+
+//problem 2
+     // double calculateAverage(int arr[], int size) {
+     //     if (size <= 0) {
+     //         return 0.0;
+     //     }
+     //     double sum = 0.0;
+     //     for (int i = 0; i < size; ++i) {
+     //         sum += arr[i];
+     //     }
+     //     return sum / size;
+     // }
+     //   int main() {
+     //     int n;
+     //     std::cout << "Enter the size of the array (n): ";
+     //     std::cin >> n;
+     //     if (n <= 0) {
+     //         std::cout << "Invalid array size." << std::endl;
+     //         return 1;
+     //     }
+     //     int arr[n];
+     //     std::cout << "Enter the elements of the array:" << std::endl;
+     //     for (int i = 0; i < n; ++i) {
+     //         std::cin >> arr[i];
+     //     }
+     //     double average = calculateAverage(arr, n);
+     //     std::cout << "The average of the array elements is: " << average << std::endl;
+
+
+
+//problem 3
+// int findMinimum(int arr[], int size) {
+//     if (size <= 0) {
+//         return INT_MAX;
 //     }
-//     if (b < 0) {
-//         return -recursiveProduct(a, -b);
+//     int minElement = arr[0];
+//     for (int i = 1; i < size; ++i) {
+//         if (arr[i] < minElement) {
+//             minElement = arr[i];
+//         }
 //     }
-//     if (a < 0) {
-//         return -recursiveProduct(-a, b);
-//     }
-//     return a + recursiveProduct(a, b - 1);
+//     return minElement;
 // }
 // int main() {
-//     int num1, num2;
-//
-//     std::cout << "Enter the first number: ";
-//     std::cin >> num1;
-//
-//     std::cout << "Enter the second number: ";
-//     std::cin >> num2;
-//
-//     int product = recursiveProduct(num1, num2);
-//     std::cout << "Product: " << product << std::endl;
-
-
-  //problem 2
-
-
-// int factorial(int n) {
-//     if (n == 0) {
+//     int n;
+//     std::cout << "Enter the size of the array (n): ";
+//     std::cin >> n;
+//     if (n <= 0) {
+//         std::cout << "Invalid array size." << std::endl;
 //         return 1;
 //     }
-//     return (n > 0) ? n * factorial(n - 1) : 0;
-// }
-// int main() {
-//     int num;
-//     std::cout << "Enter a positive integer: ";
-//     std::cin >> num;
-//     int result = factorial(num);
-//     if (num >= 0) {
-//         std::cout << "Factorial of " << num << " is: " << result << std::endl;
-//     } else {
-//         std::cout << "Undefined." << std::endl;
+//     int arr[n];
+//     std::cout << "Enter the elements of the array:" << std::endl;
+//     for (int i = 0; i < n; ++i) {
+//         std::cin >> arr[i];
 //     }
-
-
-
-     //problem 3
-
-// int fibonacci(int n) {
-//     if (n == 0)
-//         return 0;
-//      if (n == 1)
-//         return 1;
-//      else
-//         return fibonacci(n - 1) + fibonacci(n - 2);
-//     }
-//     int main () {
-//         int n;
-//         cin >> n ;
-//         cout << fibonacci(n)<< endl ;
-
-
+//     int minimum = findMinimum(arr, n);
+//     std::cout << "The minimum element is: " << minimum << std::endl;
 
 
 //problem 4
-
-// int sumNaturalNumbers(int N) {
-//
-//     if (N == 0) {
-//         return 0; // Base case: S(0) = 0
-//     } else {
-//         return N + sumNaturalNumbers(N - 1); // Recursive relation: S(N) = N + S(N - 1)
+// int indexOfLargestElement(double array[], int size) {
+//     if (size <= 0) {
+//         return -1;
 //     }
+//     int largestIndex = 0;
+//     for (int i = 1; i < size; ++i) {
+//         if (array[i] >= array[largestIndex]) {
+//             largestIndex = i;
+//         }
+//     }
+//     return largestIndex;
 // }
-//
 // int main() {
-// int n;
-//     cin >> n ;
-//     cout << sumNaturalNumbers(n) << endl ;
+//     const int SIZE = 15;
+//     double numbers[SIZE];
+//     std::cout << "Enter 15 numbers:" << std::endl;
+//     for (int i = 0; i < SIZE; ++i) {
+//         std::cin >> numbers[i];
+//     }
+//     int largestIndex = indexOfLargestElement(numbers, SIZE);
+//     std::cout << "The index of the largest element is: " << largestIndex << std::endl;
 
 
 
 //problem 5
-// void printDescending(int N) {
-//
-//     if (N > 0) {
-//         std::cout << N << std::endl;
-//         printDescending(N - 1);
+// bool strictlyEqual(const int list1[], const int list2[], int size) {
+//     if (size <= 0) {
+//         return false;
 //     }
+//     for (int i = 0; i < size; ++i) {
+//         if (list1[i] != list2[i]) {
+//             return false;
+//         }
+//     }
+//     return true;
 // }
-//
 // int main() {
-//    int N;
-//     std::cin >> N;
-//     printDescending(N);
+//     int size;
+//     std::cout << "Enter the size of the lists: ";
+//     std::cin >> size;
+//     if (size <= 0) {
+//         std::cout << "Invalid size." << std::endl;
+//         return 1;
+//     }
+//     int list1[size];
+//     int list2[size];
+//     std::cout << "Enter elements for list1:" << std::endl;
+//     for (int i = 0; i < size; ++i) {
+//         std::cin >> list1[i];
+//     }
+//     std::cout << "Enter elements for list2:" << std::endl;
+//     for (int i = 0; i < size; ++i) {
+//         std::cin >> list2[i];
+//     }
+//     if (strictlyEqual(list1, list2, size)) {
+//         std::cout << "The lists are strictly identical." << std::endl;
+//     } else {
+//         std::cout << "The lists are not strictly identical." << std::endl;
+//     }
+
+
 
 
 //problem 6
-
-// int countDigits(int n) {
-//
-//     if (n == 0) {
-//         return 0; // Base case: 0 has 0 digits
-//     } else {
-//         return 1 + countDigits(n / 10);
+// int main() {
+// int frequencies[26] = {0};
+//     char ch;
+// std::cout << "Enter a sequence of characters (enter '0' to terminate):" << std::endl;
+//     while (true) {
+//     std::cin.get(ch);
+//         if (ch == '0') {
+//         break;
+//     }
+//         if (ch >= 'a' && ch <= 'z') {
+//         frequencies[ch - 'a']++;
 //     }
 // }
-//
-// int main() {
-// int n ;
-//     std::cin >> n ;
-//     std::cout << countDigits(n) ;
+//     std::cout << "\nLetter frequencies:" << std::endl;
+// for (int i = 0; i < 26; ++i) {
+//     if (frequencies[i] > 0) {
+//         std::cout << static_cast<char>('a' + i) << ": " << frequencies[i] << std::endl;
+//     }
+// }
+
 
 
 //problem 7
+    // int main() {
+    // int n;
+    // std::cout << "Enter the number of integers: ";
+    // std::cin >> n;
+    //     if (n <= 0) {
+    //     std::cout << "Invalid number of integers." << std::endl;
+    //     return 1;
+    // }
+    //     std::vector<int> numbers(n);
+    //     std::cout << "Enter the integers:" << std::endl;
+    // for (int i = 0; i < n; ++i) {
+    //     std::cin >> numbers[i];
+    // }
+    //     std::sort(numbers.begin(), numbers.end());
+    //     std::cout << "Sorted integers:" << std::endl;
+    // for (int num : numbers) {
+    //     std::cout << num << " ";
+    // }
+    // std::cout << std::endl;
 
-// int sumOfDigits(int n) {
-//     if (n == 0) {
-//         return 0;
-//     } else {
-//         return (n % 10) + sumOfDigits(n / 10);
-//     }
-// }
-//
-// int main() {
-// int n ;
-//     std::cin >> n ;
-//     std::cout << sumOfDigits(n) ;
 
 
 //problem 8
-
-// int power(int x, int y) {
-//     if (y == 0) {
-//         return 1; // Base case: x^0 = 1
-//     } else {
-//         return x * power(x, y - 1);
-//     }
-// }
-//
 // int main() {
-// int x;
-//  std::cin >> x;
-//     int y;
-//     std::cin >> y;
-//     std::cout << power(x, y);
+//     const int rolls = 10000;
+//     int counts[6] = {0};
+//     int dieRolls[rolls];
+//     srand(static_cast<unsigned int>(time(0)));
+//     for (int i = 0; i < rolls; ++i) {
+//         int roll = rand() % 6 + 1;
+//         dieRolls[i] = roll;
+//         counts[roll - 1]++;
+//     }
+//     std::cout << "Die roll counts (10000 rolls):" << std::endl;
+//     for (int i = 0; i < 6; ++i) {
+//         std::cout << "Face " << i + 1 << ": " << counts[i] << " times" << std::endl;
+//     }
 
 
 
 //problem 9
-
-// int reverseDigits(int n, int digits) {
-//     if (n == 0) {
-//         return 0;
-//     } else {
-//         int lastDigit = n % 10;
-//         int remainingDigits = n / 10;
-//         return lastDigit * pow(10, digits - 1) + reverseDigits(remainingDigits, digits - 1);
+// void reverseArray(int arr[], int start, int end) {
+//     while (start < end) {
+//         int temp = arr[start];
+//         arr[start] = arr[end];
+//         arr[end] = temp;
+//         start++;
+//         end--;
 //     }
 // }
-//
-// int countDigits(int n) {
-//     if(n==0){
-//         return 0;
-//     }
-//     return 1 + countDigits(n/10);
-// }
-//
 // int main() {
-// int n ;
-//     cin >> n ;
-//     cout << countDigits(n) ;
+//     int myArray[] = {1, 2, 3, 4, 5};
+//     int size = sizeof(myArray) / sizeof(myArray[0]);
+//     std::cout << "Original array: ";
+//     for (int i = 0; i < size; ++i) {
+//         std::cout << myArray[i] << " ";
+//     }
+//     std::cout << std::endl;
+//     reverseArray(myArray, 0, size - 1);
+//     std::cout << "Reversed array: ";
+//     for (int i = 0; i < size; ++i) {
+//         std::cout << myArray[i] << " ";
+//     }
+//     std::cout << std::endl;
+//     int subArray[] = {10, 20, 30, 40, 50};
+//     int subSize = sizeof(subArray) / sizeof(subArray[0]);
+//     std::cout << "Original sub-array: ";
+//     for (int i = 0; i < subSize; ++i) {
+//         std::cout << subArray[i] << " ";
+//     }
+//     std::cout << std::endl;
+//     reverseArray(subArray, 1, 3);
+//     std::cout << "Reversed sub-array: ";
+//     for (int i = 0; i < subSize; ++i) {
+//         std::cout << subArray[i] << " ";
+//     }
+//     std::cout << std::endl;
 
 
 //problem 10
-// void calculateSums(int start, int end, int& evenSum, int& oddSum) {
-//     if (start > end) {
-//         return;
-//     }
-//     if (start % 2 == 0) {
-//         evenSum += start;
-//     } else {
-//         oddSum += start;
-//     }
-//     calculateSums(start + 1, end, evenSum, oddSum); // Recursive call
-// }
-// int main() {
-//     int startNum, endNum;
-//     int evenSum = 0, oddSum = 0;
-//     std::cout << "Starting number: ";
-//     std::cin >> startNum;
-//     std::cout << "Ending number: ";
-//     std::cin >> endNum;
-//     calculateSums(startNum, endNum, evenSum, oddSum);
-//     std::cout << "Sum of even numbers: " << evenSum << std::endl;
-//     std::cout << "Sum of odd numbers: " << oddSum << std::endl;
-
-
-//problem 11
-
-// void calculateSums(int start, int end, int& evenSum, int& oddSum) {
-//     if (start > end) {
-//         return;
-//     }
-//     if (start % 2 == 0) {
-//         evenSum += start;
-//     } else {
-//         oddSum += start;
-//     }
-//     calculateSums(start + 1, end, evenSum, oddSum);
-// }
-// int main() {
-//     int startNum, endNum;
-//     int evenSum = 0, oddSum = 0;
-//     std::cout << "Starting number: ";
-//     std::cin >> startNum;
-//     std::cout << "Ending number: ";
-//     std::cin >> endNum;
-//     calculateSums(startNum, endNum, evenSum, oddSum);
-//     std::cout << "Sum of even numbers: " << evenSum << std::endl;
-//     std::cout << "Sum of odd numbers: " << oddSum << std::endl;
-
-
-//problem 12
-
-void towerOfHanoi(int n, char source, char destination, char auxiliary) {
-    if (n == 1) {
-        std::cout << "Move disk 1 from " << source << " to " << destination << std::endl;
-        return;
-    }
-    towerOfHanoi(n - 1, source, auxiliary, destination);
-    std::cout << "Move disk " << n << " from " << source << " to " << destination << std::endl;
-    towerOfHanoi(n - 1, auxiliary, destination, source);
-}
 int main() {
-    int numDisks;
-    std::cout << "Enter the number of disks: ";
-    std::cin >> numDisks;
-    towerOfHanoi(numDisks, 'A', 'C', 'B');
+    int n;
+    std::cin >> n;
+    std::vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> a[i];
+    }
+    bool sorted = true;
+    for (int i = 1; i < n; ++i) {
+        if (a[i] < a[i - 1]) {
+            sorted = false;
+            break;
+        }
+    }
+    if (sorted) {
+        std::cout << "YES" << std::endl;
+    } else {
+        std::cout << "NO" << std::endl;
+    }
 
-    return 0;
+
+
+
+
+
+
+
+     return 0;
 }
